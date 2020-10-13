304dcc166abad72a258bd4a12e21e0f4f61c4744
Max Basov <aaaaasssasasa4@gmail.com>
2020-10-13 22:19:03 +0300

    done with connecting mongoDB

---
+++ src/main/java/com/scratchy/warehouse/config/MongoConfig.java
@@ -0,0 +1,21 @@
+package com.scratchy.warehouse.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    public MongoClient mongoClient(){
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(){
+        return new MongoTemplate(mongoClient(), "itemDB");
+    }
+
+}
--- src/main/java/com/scratchy/warehouse/model/Item.java
+++ src/main/java/com/scratchy/warehouse/model/Item.java
@@ -1,20 +1,32 @@
 package com.scratchy.warehouse.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
 @Data
+@Document(collection = "itemData")
 public class Item {
     
     @Id
-    private int id;
+    private String id;
+    
+    @Field(value = "name")
     private String name;
+    
+    @Field(value = "count")
     private int count;
-
 
     public Item(String name, int count) {
         this.name = name;
         this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " + name + ", count: " + count;
     }
 }
